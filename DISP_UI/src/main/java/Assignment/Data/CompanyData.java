e55dcafc0bd15dcf9a17006d75d4d753c0f18485
Domidev77 <78223866+Domidev77@users.noreply.github.com>
2023-04-29 00:59:32 +0200

    add wip ui more work

--- DISP_UI/src/main/java/Assignment/Data/CompanyData.java
+++ DISP_UI/src/main/java/Assignment/Data/CompanyData.java
@@ -1,4 +1,24 @@
 package Assignment.Data;
 
+import Assignment.Model.Customer;
+import Assignment.Model.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class CompanyData {
+    Customer customer = new Customer();
+
+    List<Customer> listOfCustomers = Arrays.asList(
+            new Customer("John","Baker", "7453f23a", true,
+                    "BS3 6AT", "07532136543","john.baker@gmail.com","7 Victoria street")
+    );
+
+    public ObservableList<Product> loadData(){
+
+        ObservableList<Product> observableList = FXCollections.observableArrayList();
+        CompanyData accommodationData = new CompanyData();
+
 }
